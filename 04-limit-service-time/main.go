d0325bcd9e9cc1bbd283380d1de3004490d398d7
nuchemka <nuchemka@gmail.com>
2020-07-13 17:47:23 +0100

    finished exercise 04

--- 04-limit-service-time/main.go
+++ 04-limit-service-time/main.go
@@ -1,9 +1,16 @@
 package main
 
+import (
+	"sync"
+	"time"
+
+)
+
 // User defines the UserModel.
 // Use this to check whether a User is a Premium user or not.
 
 type User struct {
+	mu sync.Mutex
 	ID        int
 	IsPremium bool
 	TimeUsed  int64 // in seconds
@@ -11,10 +18,55 @@ type User struct {
 
 // HandleRequest runs the processes requested by users.
 // Returns false if a process had to be killed
-func HandleRequest(process func(), u *User) bool {
-	process()
-	return true
+func worker(done chan struct{},process func()) {
+	  process()
+	close(done)
 }
+
+func HandleRequest(process func(), u *User) bool {
+
+
+done := make(chan struct{})
+
+	if u.IsPremium == false {
+
+		if u.TimeUsed >= 10 {
+
+			return false
+		} else {
+
+			timeDuration := 10*time.Second - time.Duration(u.TimeUsed*1E9)
+			startTime := time.Now()
+
+			timer1 := time.NewTimer(timeDuration)
+
+			go worker(done, process)
+			for {
+				select {
+				case <-timer1.C:
+					u.mu.Lock()
+
+					u.TimeUsed = 10;
+					u.mu.Unlock()
+					return false
+
+				case <-done:
+					 u.mu.Lock()
+
+					u.TimeUsed = u.TimeUsed + int64(time.Now().Sub(startTime).Seconds())
+					u.mu.Unlock()
+					return true
+				}
+			}
+
+		}
+	} else {
+			   process()
+			  return true
+		  }
+
+	}
+
 
 func main() {
 	RunMockServer()
